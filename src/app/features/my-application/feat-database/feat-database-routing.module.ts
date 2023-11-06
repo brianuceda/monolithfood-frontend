546630bf379a47a64bbbd3b19a356d4de17cc5e7
Kiridepapel <monalek0622@hotmail.com>
2023-11-06 02:20:01 -0500

    feat(dev): block routes by roles on token

--- src/app/features/my-application/feat-database/feat-database-routing.module.ts
+++ src/app/features/my-application/feat-database/feat-database-routing.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ListFoodsComponent } from './components/list-foods/list-foods.component';
 import { FavouritesComponent } from './components/favourites/favourites.component';
+import { PermissionGuard } from 'src/app/core/guards/permission.guard';
 
 const routes: Routes = [
   { path: '', component: ListFoodsComponent },
-  { path: 'favourites', component: FavouritesComponent },
+  {
+    path: 'favourites',
+    component: FavouritesComponent,
+    canActivate: [PermissionGuard], // Añadir el guard aquí
+    data: { roles: ['ROLE_ADMIN', 'ROLE_VIP'] }, // Añadir los roles requeridos aquí
+  },
 ];
 
 @NgModule({
