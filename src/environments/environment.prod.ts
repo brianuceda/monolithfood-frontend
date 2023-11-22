768be60f7a837c3b2577ff5ca8912dffc6a4b5fc
Kiridepapel <monalek0622@hotmail.com>
2023-11-22 15:19:52 -0500

    feat(dev): prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,14 @@
 export const environment = {
-  oauthUrl: 'http://34.41.73.58/oauth2/authorization',
-  oauthUrlMicrosoft: 'https://monolithcards.site/oauth2/authorization',
-
   // ! Production
   production: true,
   api: 'https://monolithcards.site',
+  oauthUrl: 'http://34.41.73.58/oauth2/authorization',
+  oauthUrlMicrosoft: 'https://monolithcards.site/oauth2/authorization',
   // ! Development
   // production: false,
   // api: 'http://localhost:8080',
+  // oauthUrl: 'http://localhost:8080/oauth2/authorization',
+  // oauthUrlMicrosoft: 'http://localhost:8080/oauth2/authorization',
 
   rscAuth: '/auth',
   rscUsers: '/user',
