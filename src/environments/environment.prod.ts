8b11ea3d54b46c101b7c1b4013ea40d4a5133b9a
Kiridepapel <monalek0622@hotmail.com>
2023-12-06 13:46:25 -0500

    feat(dev): change frontend domain

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,14 +1,8 @@
 export const environment = {
-  // ! Production
   production: true,
   api: 'https://monolithcards.site',
-  oauthUrl: 'http://34.41.73.58/oauth2/authorization',
+  oauthUrl: 'http://34.135.121.150/oauth2/authorization',
   oauthUrlMicrosoft: 'https://monolithcards.site/oauth2/authorization',
-  // ! Development
-  // production: false,
-  // api: 'http://localhost:8080',
-  // oauthUrl: 'http://localhost:8080/oauth2/authorization',
-  // oauthUrlMicrosoft: 'http://localhost:8080/oauth2/authorization',
 
   rscAuth: '/auth',
   rscUsers: '/user',
